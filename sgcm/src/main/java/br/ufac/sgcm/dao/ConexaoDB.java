371efba83d64568d3a870175e3905c66e158db26
Manoel <mlimeira@ic.uff.br>
2024-09-23 18:30:26 -0500

    Arquivo de conexão com BD

---
+++ sgcm/src/main/java/br/ufac/sgcm/dao/ConexaoDB.java
@@ -0,0 +1,21 @@
+package main.java.br.ufac.sgcm.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexaoDB {
+    private String usuario = "root";
+    private String senha = "root";
+    private String url = "jdbc:msyql://localhost/sgcm";
+    Connection con;
+
+    public Connection getConexao() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, usuario, senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
